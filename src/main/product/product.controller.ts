346ee9b6bf88c10951992dce0de7a2a538863ddb
azmain-siam <siamhossain2700@gmail.com>
2025-07-08 06:43:43 +0600

    fix: some issues

--- src/main/product/product.controller.ts
+++ src/main/product/product.controller.ts
@@ -1,14 +1,31 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Multer } from 'multer';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
+  @UseInterceptors(FileInterceptor('file'))
+  create(
+    @Body() createProductDto: CreateProductDto,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    console.log(file);
     return this.productService.create(createProductDto);
   }
 
--- src/main/product/product.module.ts
+++ src/main/product/product.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
+  imports: [
+    MulterModule.register({
+      dest: './uploads',
+    }),
+  ],
   controllers: [ProductController],
   providers: [ProductService],
 })
